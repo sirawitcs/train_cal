9d21f964ac059dedd25d37f41d5a796a7d63f667
sirawit churmsuk <sirawit.churm@gmail.com>
2026-06-16 11:55:06 +0700

    init project

---
+++ train_cal_b/src/main/java/com/example/train_cal_b/TrainCalBApplication.java
@@ -0,0 +1,13 @@
+package com.example.train_cal_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainCalBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainCalBApplication.class, args);
+	}
+
+}
---
+++ train_cal_b/src/test/java/com/example/train_cal_b/TrainCalBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.train_cal_b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainCalBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
